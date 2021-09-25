ecda9216edf21e0b7e219e61245bef90a579364a
hamidsultanzadeh <sultanzade.06@mail.ru>
2021-09-25 14:28:34 +0400

    DB : V0.0.1_USER_TABLES.SQL

---
+++ src/test/java/com/webperside/blogsite/BlogSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webperside.blogsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
