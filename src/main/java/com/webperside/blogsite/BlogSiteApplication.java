4c261f441a41e2178f4ecd162ba570de4f561a3a
hamidsultanzadeh <sultanzade.06@mail.ru>
2021-09-25 21:06:30 +0400

    create user tables

--- src/main/java/com/webperside/blogsite/BlogSiteApplication.java
+++ src/main/java/com/webperside/blogsite/BlogSiteApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogSiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogSiteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSiteApplication.class, args);
+    }
 }
